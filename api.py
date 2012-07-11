2d78711001892d53c01f8c21d08528503f637b4b
Martin Kleinschrodt <martin@maklesoft.com>
2012-07-11 17:42:01 +0200

    Move validate_user from views module to api module; Define api urls in handball app instead of project; implement user exists api endpoint

--- api.py
+++ api.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 from tastypie.authorization import DjangoAuthorization, Authorization
 from tastypie.authentication import BasicAuthentication, Authentication, ApiKeyAuthentication
 from handball.authorization import ManagerAuthorization
+from django.contrib.auth import authenticate
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, HttpResponseBadRequest
+from tastypie.serializers import Serializer
+from tastypie.utils.mime import determine_format
 
 
 class UnionResource(ModelResource):
@@ -142,5 +146,86 @@ class EventResource(ModelResource):
     class Meta:
         queryset = Event.objects.all()
         authorization = Authorization()
-        authentication = ApiKeyAuthentication()
+        authentication = Authentication()
         include_resource_uri = False
+
+
+"""
+Non-resource api endpoints
+"""
+
+
+def validate_user(request):
+    """
+    Checks a user's basic auth credentials and, if valid, returns the users data
+    """
+
+    # if not request.META.get('HTTP_AUTHORIZATION'):
+    #     return HttpResponseBadRequest('No HTTP_AUTHORIZATION header found')
+
+    # try:
+    #     (auth_type, data) = request.META['HTTP_AUTHORIZATION'].split()
+    #     if auth_type.lower() != 'basic':
+    #         return HttpResponseBadRequest('Wrong auth type. Use basic auth!')
+    #     user_pass = base64.b64decode(data)
+    # except:
+    #     return HttpResponseBadRequest('Could not decode auth credentials.')
+
+    # bits = user_pass.split(':', 1)
+
+    # if len(bits) != 2:
+    #     return HttpResponseBadRequest('Could not decode auth credentials.')
+
+    # user = authenticate(username=bits[0], password=bits[1])
+
+    username = request.POST['username']
+    password = request.POST['password']
+
+    if not username or not password:
+        return HttpResponseBadRequest()
+
+    user = authenticate(username=username, password=password)
+
+    if user is None or not user.is_active:
+        return HttpResponseNotFound('User does not exist or password incorrect.')
+
+    person = user.get_profile()
+
+    person_resource = PersonResource()
+    bundle = person_resource.build_bundle(obj=person, request=request)
+    person_resource.full_dehydrate(bundle)
+    bundle.data['api_key'] = user.api_key.key
+
+    return HttpResponse(person_resource.serialize(None, bundle, 'application/json'))
+
+
+def user_exists(request):
+    username = request.GET['username']
+    email = request.GET['email']
+    username_exists = True
+    email_exists = True
+
+    try:
+        User.objects.get(username=username)
+    except User.DoesNotExist:
+        username_exists = False
+    except User.MultipleObjectsReturned:
+        email_exists = True
+
+    try:
+        User.objects.get(email=email)
+    except User.DoesNotExist:
+        email_exists = False
+    except User.MultipleObjectsReturned:
+        email_exists = True
+
+    data = {
+        'username': username_exists,
+        'email': email_exists
+    }
+
+    serializer = Serializer()
+
+    format = determine_format(request, serializer, default_format='application/json')
+
+    return HttpResponse(serializer.serialize(data, format, {}))
--- urls.py
+++ urls.py
@@ -1,10 +1,29 @@
 from django.conf.urls.defaults import *
+from tastypie.api import Api
+from handball.api import *
 
+
+v1_api = Api(api_name='v1')
+v1_api.register(UnionResource())
+v1_api.register(ClubResource())
+v1_api.register(TeamResource())
+v1_api.register(UserResource())
+v1_api.register(PersonResource())
+v1_api.register(GameResource())
+v1_api.register(LeagueResource())
 
 urlpatterns = patterns('handball.views',
     (r'^$', 'index'),
     (r'^auth/signup/$', 'sign_up'),
     (r'^auth/activate/([abcdef0123456789]+)$', 'activate'),
-    (r'^thanks/$', 'thanks'),
-    (r'^auth/validate/$', 'validate_user')
+    (r'^thanks/$', 'thanks')
+)
+
+urlpatterns += patterns('', (r'^api/', include(v1_api.urls)))
+
+# Non-resource api endpoints
+urlpatterns += patterns('handball.api',
+    (r'^api/v1/auth/validate/$', 'validate_user'),
+    (r'^api/v1/auth/exists/$', 'user_exists')
+    #(r'^api/v1/auth/signup/$', 'sign_up')
 )
