3b3c43439abc2508a16e6b86d2ea970577bc8c37
Martin Kleinschrodt <martin@engaginglab.com>
2012-08-04 20:21:44 +0200

    Fix urls - again.

--- urls.py
+++ urls.py
@@ -20,9 +20,9 @@ v1_api.register(EventTypeResource())
 v1_api.register(GamePlayerRelationResource())
 v1_api.register(TeamPlayerRelationResource())
 
-urlpatterns = patterns('', (r'^api/', include(v1_api.urls)))
+urlpatterns = patterns('', (r'^', include(v1_api.urls)))
 
 # Non-resource api endpoints
 urlpatterns += patterns('handball.api',
-    (r'^api/v1/unique/$', 'is_unique'),
+    (r'^v1/unique/$', 'is_unique'),
 )
